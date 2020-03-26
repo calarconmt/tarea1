507f3e410ecc3ba07d8689625e83afd7932616ab
Claudio Andres Alarcón Carvajal <calarcon@miningtag.cl>
2020-03-26 16:20:01 -0300

    ads

--- src/app/interfaces/endpointdatas.ts
+++ src/app/interfaces/endpointdatas.ts
@@ -1,7 +1,7 @@
-import { Machstatuslog } from './machstatuslog';
+import { IMachstatuslog } from './machstatuslog';
 
 // tslint:disable-next-line: class-name
 export interface endpointdatas {
-    statusLog: Array<Machstatuslog>;
+    statusLog: Array<IMachstatuslog>;
     statusMachine: any;
 }
